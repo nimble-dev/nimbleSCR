0be7d27ad10f18cc96adf00684a68c8009eb9397
danielturek <danielturek@gmail.com>
2020-07-07 11:31:07 -0400

    DT added distributions registrations to zzz.R

--- nimbleSCR/R/zzz.R
+++ nimbleSCR/R/zzz.R
@@ -12,6 +12,42 @@
                 )
             ),
             verbose = FALSE)
-        
+
+        registerDistributions(
+            list(
+                dHabitatMask = list(
+                    BUGSdist = 'dHabitatMask(s, xmax, xmin, ymax, ymin, habitatMask)',
+                    types = c('s = double(1)', 'habitatMask = double(2)'),
+                    discrete = TRUE,
+                    mixedSizes = TRUE,
+                    pqAvail = FALSE
+                )
+            ),
+            verbose = FALSE)
+
+        registerDistributions(
+            list(
+                dbinom_SparseLocalSCR = list(
+                    BUGSdist = 'dbinom_SparseLocalSCR(detNums, detIndices, size, p0, sigma, s, trapCoords, localTrapsIndices, localTrapsNum, resizeFactor, habitatGrid, indicator)',
+                    types = c('value = double(1)', 'detIndices = double(1)', 'size = double(1)', 's = double(1)', 'trapCoords = double(2)', 'localTrapsIndices = double(2)', 'localTrapsNum = double(1)', 'habitatGrid = double(2)'),
+                    discrete = TRUE,
+                    mixedSizes = TRUE,
+                    pqAvail = FALSE
+                )
+            ),
+            verbose = FALSE)
+
+        registerDistributions(
+            list(
+                dbinom_vector = list(
+                    BUGSdist = 'dbinom_vector(size, prob)',
+                    types = c('value = double(1)', 'size = double(1)', 'prob = double(1)'),
+                    discrete = TRUE,
+                    mixedSizes = FALSE,
+                    pqAvail = FALSE
+                )
+            ),
+            verbose = FALSE)
+
     })
 }
