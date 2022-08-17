7caa13a42a0b1818781e19842d291c17bdf931a4
soumenstat89 <soumenstat89@gmail.com>
2022-08-17 15:59:25 +0530

    Changing HRA_nimble to getHomeRangeArea

--- nimbleSCR/R/getHomeRangeArea.R
+++ nimbleSCR/R/getHomeRangeArea.R
@@ -4,19 +4,19 @@
 #' @title Computation of home range radius and area
 #'
 #' @description
-#' \code{HRA_nimble} returns the estimates of home range radius and area for a given set of parameters with respect to a specified detection function using bisection algorithm. The following circular detection functions are available to use in nimbleSCR: half-normal(HN, detfun = 0), half-normal plateau (HNP, detfun = 1), exponential (EXP, detfun = 2), asymmetric logistic (AL, detfun = 3), bimodal (BI, detfun = 4) and donut (DN, detfun = 5).
+#' \code{getHomeRangeArea} returns the estimates of home range radius and area for a given set of parameters with respect to a specified detection function using bisection algorithm. The following circular detection functions are available to use in nimbleSCR: half-normal(HN, detFun = 0), half-normal plateau (HNP, detFun = 1), exponential (EXP, detFun = 2), asymmetric logistic (AL, detFun = 3), bimodal (BI, detFun = 4) and donut (DN, detFun = 5).
 #' 
-#' @name HRA_nimble
+#' @name getHomeRangeArea
 #' 
-#' @param param \code{Vector} or \code{matrix} (parameters in columns) of values for different parameters corresponding to the specified detection function.
-#' @param detfun \code{Numeric} variable denoting the type of detection function. 0 = Half-normal (HN), 1 = Half-normal plateau (HNP), 2 = Exponential (EXP), 3 = Asymmetric logistic (AL), 4 = Bimodal (BI), 5 = Donut (DN).
-#' @param pr \code{Numeric}  variable denoting the quantile probability to compute the home range radius.
+#' @param x \code{Vector} or \code{matrix} (parameters in columns) of values for different parameters corresponding to the specified detection function.
+#' @param detFun \code{Numeric} variable denoting the type of detection function. 0 = Half-normal (HN), 1 = Half-normal plateau (HNP), 2 = Exponential (EXP), 3 = Asymmetric logistic (AL), 4 = Bimodal (BI), 5 = Donut (DN).
+#' @param prob \code{Numeric}  variable denoting the quantile probability to compute the home range radius.
 #' @param d \code{Numeric} variable giving an initial value of the radius.
 #' @param xlim \code{Vector} of length 2 giving the range along x-axis.
 #' @param ylim \code{Vector} of length 2 giving the range along y-axis.
-#' @param ng \code{Numeric} variable denoting the number of breaks along an axis.
+#' @param nBreaks \code{Numeric} variable denoting the number of breaks along an axis.
 #' @param tol \code{Numeric} variable denoting the allowed tolerance in the radius estimate.
-#' @param niter \code{Numeric} variable giving the maximum number of iterations in bisection algorithm.
+#' @param nIter \code{Numeric} variable giving the maximum number of iterations in bisection algorithm.
 #'
 #' @author Soumen Dey
 #' 
@@ -31,20 +31,20 @@
 #' # https://github.com/nimble-dev/nimbleSCR/blob/master/nimbleSCR/vignettes/
 #' # Vignette name: Fit_SCR_models_with_dbinomLocal_HNP_and_HomeRangeRadiusComputation.rmd
 #' 
-#' # HALF-NORMAL PLATEAU FUNCTION (HNP, detfun = 1) 
+#' # HALF-NORMAL PLATEAU FUNCTION (HNP, detFun = 1) 
 #' habitatMask <- matrix(1, nrow = 30, ncol= 30, byrow = TRUE)
 #' 
-#' pr <- 0.95
+#' prob <- 0.95
 #' paramnames.hr <- c("HRradius", "HRarea")
 #' sigma <- 1
 #' w <- 1.5
 #' params <- c(sigma, w)
 #' names(params) <- c("sigma", "w")
-#' HRAnim <- HRA_nimble( param = params, detfun = 1, pr = pr, d = 6,
+#' HRAnim <- getHomeRangeArea( x = params, detFun = 1, prob = prob, d = 6,
 #'                       xlim = c(0, dim(habitatMask)[2]), ylim = c(0, dim(habitatMask)[1]),
-#'                       ng = 800, tol = 1E-5, niter = 2000)
+#'                       nBreaks = 800, tol = 1E-5, nIter = 2000)
 #' 
-#' # Different values of argument "detfun"
+#' # Different values of argument "detFun"
 #' # 0 = Half-normal (HN), 1 = Half-normal plateau (HNP), 2 = Exponential (EXP),
 #' # 3 = Aysmmetric logistic (AL), 4 = Bimodal (BI), 5 = Donut (DN).
 #' HR.hnp <- c(HRAnim$run())
@@ -53,9 +53,9 @@
 #' # FASTER HRA COMPUTATION USING NIMBLE
 #' samples <- cbind(rgamma(n = 500, shape = 1, rate = 1), rgamma(n = 500, shape = 1.5, rate = 1))
 #' colnames(samples) <- c("sigma", "w")
-#' HRAnim.mat <- HRA_nimble(param = samples, detfun = 1, pr = pr, d = 6, 
+#' HRAnim.mat <- getHomeRangeArea(x = samples, detFun = 1, prob = prob, d = 6, 
 #'                          xlim = c(0, dim(habitatMask)[2]), ylim = c(0, dim(habitatMask)[1]),
-#'                          ng = 800, tol = 1E-5, niter = 2000)
+#'                          nBreaks = 800, tol = 1E-5, nIter = 2000)
 #' 
 #' cHRAnim.arr <- compileNimble(HRAnim.mat, resetFunctions = TRUE)
 #' 
@@ -72,47 +72,47 @@
 #' cat("Numerical estimates using MCMC samples: \n", sep = "")
 #' print(HRest)
 #' 
-#' # HALF-NORMAL FUNCTION (HN, detfun = 0) 
+#' # HALF-NORMAL FUNCTION (HN, detFun = 0) 
 #' sigma = 2
 #' params <- c(sigma)
 #' names(params) <- c("sigma")
 #' 
-#' HRAnim <- HRA_nimble(param = params, detfun = 0, pr = pr, d = 6, 
+#' HRAnim <- getHomeRangeArea(x = params, detFun = 0, prob = prob, d = 6, 
 #'                      xlim = c(0, dim(habitatMask)[2]), ylim = c(0, dim(habitatMask)[1]),
-#'                      ng = 800, tol = 1E-5, niter = 2000)
+#'                      nBreaks = 800, tol = 1E-5, nIter = 2000)
 #' 
 #' HR.hn <- c(HRAnim$run())
 #' names(HR.hn) <- paramnames.hr
 #' print(HR.hn)
 #' 
-#' # Exponential (EXP, detfun = 2) 
+#' # Exponential (EXP, detFun = 2) 
 #' 
 #' sigma = 2
 #' params <- c(sigma)
 #' names(params) <- c("sigma")
-#' HRAnim <- HRA_nimble(param = params, detfun = 2, pr = pr, d = 6, 
+#' HRAnim <- getHomeRangeArea(x = params, detFun = 2, prob = prob, d = 6, 
 #'                      xlim = c(0, dim(habitatMask)[2]), ylim = c(0, dim(habitatMask)[1]),
-#'                      ng = 800, tol = 1E-5, niter = 2000)
+#'                      nBreaks = 800, tol = 1E-5, nIter = 2000)
 #' HR.ex <- c(HRAnim$run())
 #' names(HR.ex) <- paramnames.hr
 #' print(HR.ex)
 #' 
-#' # Asymmetric logistic (AL, detfun = 3) 
+#' # Asymmetric logistic (AL, detFun = 3) 
 #' 
 #' sigma = 2
 #' alpha.a = 5 
 #' alpha.b = 1
 #' params <- c(sigma, alpha.a, alpha.b)
 #' names(params) <- c("sigma", "alpha.a", "alpha.b")
-#' HRAnim <- HRA_nimble(param = params, detfun = 3, pr = pr, d = 6, 
+#' HRAnim <- getHomeRangeArea(x = params, detFun = 3, prob = prob, d = 6, 
 #'                      xlim = c(0, dim(habitatMask)[2]), ylim = c(0, dim(habitatMask)[1]),
-#'                      ng = 800, tol = 1E-5, niter = 2000)
+#'                      nBreaks = 800, tol = 1E-5, nIter = 2000)
 #' HR.al <- c(HRAnim$run())
 #' names(HR.al) <- paramnames.hr
 #' print(HR.al)
 #' 
 #' 
-#' # Bimodal (BI, detfun = 4) 
+#' # Bimodal (BI, detFun = 4) 
 #' 
 #' p0.a = 0.25
 #' sigma.a = 0.5
@@ -121,23 +121,23 @@
 #' w = 2
 #' params <- c(sigma.a, sigma.b, p0.a, p0.b, w)
 #' names(params) <- c("sigma.a", "sigma.b", "p0.a", "p0.b", "w")
-#' HRAnim <- HRA_nimble(param = params, detfun = 4, pr = pr, d = 6, 
+#' HRAnim <- getHomeRangeArea(x = params, detFun = 4, prob = prob, d = 6, 
 #'                      xlim = c(0, dim(habitatMask)[2]), ylim = c(0, dim(habitatMask)[1]),
-#'                      ng = 800, tol = 1E-5, niter = 2000)
+#'                      nBreaks = 800, tol = 1E-5, nIter = 2000)
 #' HR.bi <- c(HRAnim$run())
 #' names(HR.bi) <- paramnames.hr
 #' print(HR.bi)
 #' 
-#' # Donut (DN, detfun = 5) 
+#' # Donut (DN, detFun = 5) 
 #' 
 #' sigma.a = 1.5 
 #' sigma.b = 1 
 #' w = 1 
 #' params <- c(sigma.a, sigma.b, w)
 #' names(params) <- c("sigma.a", "sigma.b", "w")
-#' HRAnim <- HRA_nimble(param = params, detfun = 5, pr = pr, d = 6, 
+#' HRAnim <- getHomeRangeArea(x = params, detFun = 5, prob = prob, d = 6, 
 #'                      xlim = c(0, dim(habitatMask)[2]), ylim = c(0, dim(habitatMask)[1]),
-#'                      ng = 800, tol = 1E-5, niter = 2000)
+#'                      nBreaks = 800, tol = 1E-5, nIter = 2000)
 #' HR.dn <- c(HRAnim$run())
 #' names(HR.dn) <- paramnames.hr
 #' print(HR.dn)
@@ -147,83 +147,83 @@
 #' @export
 NULL
 
-#' @rdname HRA_nimble
+#' @rdname getHomeRangeArea
 #' @export
 #' 
 #Bisection algorithm
-HRA_nimble <- nimbleFunction(
-  setup = function(param=c(2), detfun=0, pr = 0.95, d = 6,
+getHomeRangeArea <- nimbleFunction(
+  setup = function(x=c(2), detFun=0, prob = 0.95, d = 6,
                    xlim=c(0,30), ylim=c(0,30), 
-                   ng=800, tol=1E-5, niter=2000){
+                   nBreaks=800, tol=1E-5, nIter=2000){
     s <- c(sum(xlim)/2, sum(ylim)/2) # center of the rectangular grid
-    x1 <- rep(seq(xlim[1], xlim[2], length.out = ng), ng) # vector (ng^2x 1)
-    x2.temp <- seq(ylim[1], ylim[2], length.out = ng)
-    x2 <- nimNumeric(value = 0, length = ng^2, init = TRUE) 
-    for(i in 1:ng){
-      x2[((i-1)*ng+1):(i*ng)] <- rep(x2.temp[i], ng) # NIMBLE
+    x1 <- rep(seq(xlim[1], xlim[2], length.out = nBreaks), nBreaks) # vector (nBreaks^2x 1)
+    x2.temp <- seq(ylim[1], ylim[2], length.out = nBreaks)
+    x2 <- nimNumeric(value = 0, length = nBreaks^2, init = TRUE) 
+    for(i in 1:nBreaks){
+      x2[((i-1)*nBreaks+1):(i*nBreaks)] <- rep(x2.temp[i], nBreaks) # NIMBLE
     }
-    n <- length(x1) # ng^2
+    n <- length(x1) # nBreaks^2
     D <- sqrt((s[1] - x1[1:n])^2 + (s[2] - x2[1:n])^2 ) # vector (n x 1) # NIMBLE
     resolution <- min(diff(x1[1:10])) # Resolution of the grid
     
-    if(!is.matrix(param)){ param <- t(as.matrix(param))}
+    if(!is.matrix(x)){ x <- t(as.matrix(x))}
     
   }, #setup
   run = function(){
     
-    nrows <- dim(param)[1]
+    nrows <- dim(x)[1]
     # nrows <- length(p0Vec)
     out <- matrix(NA, nrows, 2)
     
     for(this.row in 1:nrows ){
       if (this.row%%1000 == 0)  cat('..... row #', this.row, '\n')
       
-      if(detfun == 0){ #'HN'
+      if(detFun == 0){ #'HN'
         # paramnames = c('sigma') # PARAMETER NAMES IN THEIR ORDER OF USE
-        sigma <- param[this.row, 1] # Scale parameter 
+        sigma <- x[this.row, 1] # Scale parameter 
         p <- exp(-D[1:n]*D[1:n]/(2*sigma*sigma)) # vector (n x 1)
       }
-      if(detfun == 1){ #  'HNP'
+      if(detFun == 1){ #  'HNP'
         # paramnames = c('sigma', 'w') # PARAMETER NAMES IN THEIR ORDER OF USE
-        sigma <- param[this.row, 1] # Scale parameter 
-        w <- param[this.row, 2] # Length of plateau
+        sigma <- x[this.row, 1] # Scale parameter 
+        w <- x[this.row, 2] # Length of plateau
         p <- nimNumeric(length = n, value = 1, init = TRUE)
         for(i in 1:n){
           if(D[i] > w) {p[i] <- exp(-(D[i] - w)*(D[i] - w)/(2*sigma*sigma))}
         }
       }
-      if(detfun == 2){ # 'EXP'
+      if(detFun == 2){ # 'EXP'
         # paramnames = c('sigma') # PARAMETER NAMES IN THEIR ORDER OF USE
-        sigma <- param[this.row, 1] # Scale parameter 
+        sigma <- x[this.row, 1] # Scale parameter 
         p <- exp(-D[1:n]/sigma) # vector (n x 1)
       }
-      if(detfun == 3){ #'AL'
+      if(detFun == 3){ #'AL'
         # paramnames = c('sigma', 'alpha.a', 'alpha.b') # PARAMETER NAMES IN THEIR ORDER OF USE
-        sigma <- param[this.row, 1] # Scale parameter 
-        alpha.a <- param[this.row, 2]
-        alpha.b <- param[this.row, 3]
+        sigma <- x[this.row, 1] # Scale parameter 
+        alpha.a <- x[this.row, 2]
+        alpha.b <- x[this.row, 3]
         Anuslope <- (2*abs(alpha.a)*alpha.b)/(1+alpha.b)
         fx <- 1/ (1+(D[1:n]/sigma)^Anuslope)
         dens <- 1+fx[1:n]*((D[1:n]/sigma)^(alpha.a))+(1-fx[1:n])*((D[1:n]/sigma)^(alpha.a*alpha.b))
         p <- 1/dens[1:n]
       }
-      if(detfun == 4){ #'BI'
+      if(detFun == 4){ #'BI'
         # paramnames = c('sigma', 'sigma.b', 'p0', 'p0.b', 'w') # PARAMETER NAMES IN THEIR ORDER OF USE
-        sigma.a <- param[this.row, 1] # Scale parameter of the first peak 
-        sigma.b <- param[this.row, 2] # Scale parameter of the second peak 
-        p0.a <- param[this.row, 3] # Baseline detection probability of the first peak 
-        p0.b <- param[this.row, 4] # Baseline detection probability of the second peak 
-        w <- param[this.row, 5] # Distance between the home-range center and the "doughnut" center
+        sigma.a <- x[this.row, 1] # Scale parameter of the first peak 
+        sigma.b <- x[this.row, 2] # Scale parameter of the second peak 
+        p0.a <- x[this.row, 3] # Baseline detection probability of the first peak 
+        p0.b <- x[this.row, 4] # Baseline detection probability of the second peak 
+        w <- x[this.row, 5] # Distance between the home-range center and the "doughnut" center
         densa <- p0.a *  exp(-D[1:n]*D[1:n]/(2.0*sigma.a*sigma.a)) 
         densb <-  p0.b * exp(-(D[1:n]-w)*(D[1:n]-w)/(2.0*sigma.b*sigma.b))
         p <- densa[1:n] + densb[1:n]
         
       }
-      if(detfun == 5){ #'DN'
+      if(detFun == 5){ #'DN'
         # paramnames = c('sigma', 'sigma.b', 'w') # PARAMETER NAMES IN THEIR ORDER OF USE
-        sigma.a <- param[this.row, 1] # Scale parameter of the left tail
-        sigma.b <- param[this.row, 2] # Scale parameter of the right tail
-        w <- param[this.row, 3] # Distance between the activity centre and the "donut" centre
+        sigma.a <- x[this.row, 1] # Scale parameter of the left tail
+        sigma.b <- x[this.row, 2] # Scale parameter of the right tail
+        w <- x[this.row, 3] # Distance between the activity centre and the "donut" centre
         p <- nimNumeric(length = n, value = 0, init = TRUE)
         for(i in 1:n){
           if(D[i] <= w) {p[i] <- exp(-(D[i] - w)*(D[i] - w)/(2*sigma.a*sigma.a))}
@@ -241,32 +241,32 @@ HRA_nimble <- nimbleFunction(
         if(D[i] <= d) temp <- temp + psi[i]
       }
       iter <- 1 
-      while(iter <= niter &
+      while(iter <= nIter &
             abs(d.upper-d.lower) > d*tol &
             abs(d.upper-d.lower) > resolution){
         
-        if(temp >= pr){
+        if(temp >= prob){
           d <<- (d.lower+d)/2
           temp <- 0
           for(i in 1:n){
             if(D[i] <= d) temp <- temp + psi[i]
           }
-          if(temp < pr) d.lower <- d
-          if(temp >= pr) d.upper <- d ## NEW
+          if(temp < prob) d.lower <- d
+          if(temp >= prob) d.upper <- d ## NEW
         }
-        if(temp < pr){
+        if(temp < prob){
           d <<- (d.upper+d)/2
           temp <- 0
           for(i in 1:n){
             if(D[i] <= d) temp <- temp + psi[i]
           }
-          if(temp < pr) d.lower <- d ## NEW
-          if(temp >= pr) d.upper <- d
+          if(temp < prob) d.lower <- d ## NEW
+          if(temp >= prob) d.upper <- d
         }
         iter <- iter + 1
         
       }#while
-      if(temp < pr) {d <<- d*(1+tol) }
+      if(temp < prob) {d <<- d*(1+tol) }
       radius <- d
       area <- pi * radius^2
       out[this.row, 1:2] <- c(radius, area)
